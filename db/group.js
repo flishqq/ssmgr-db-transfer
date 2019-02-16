a940ea98a506b1709924ccc7256b896fe47cd826
flishqq <mybanner@qq.com>
2019-02-16 17:41:47 +0800

    Update group.js

--- db/group.js
+++ db/group.js
@@ -2,4 +2,7 @@ module.exports = function(table) {
   table.increments('id');
   table.string('name');
   table.string('comment');
+  table.integer('showNotice').defaultTo(1);
+  table.string('order');
+  table.integer('multiAccount').defaultTo(0);
 }
