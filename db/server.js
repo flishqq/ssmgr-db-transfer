8520559789f27c4009b1d298d4a8dc5f6b553639
flishqq <mybanner@qq.com>
2019-02-16 17:51:04 +0800

    Update server.js

--- db/server.js
+++ db/server.js
@@ -1,11 +1,15 @@
 module.exports = function(table) {
-  table.increments('id');
-  table.string('name');
-  table.string('host');
-  table.integer('port');
-  table.string('password');
-  table.float('scale').defaultTo(1);
-  table.string('method').defaultTo('aes-256-cfb');
-  table.string('comment').defaultTo('');
-  table.integer('shift').defaultTo(0);
+    table.increments('id');
+    table.string('type').defaultTo('Shadowsocks');
+    table.string('name');
+    table.string('host');
+    table.integer('port');
+    table.string('password');
+    table.float('scale').defaultTo(1);
+    table.string('method').defaultTo('aes-256-cfb');
+    table.string('comment').defaultTo('');
+    table.integer('shift').defaultTo(0);
+    table.string('key');
+    table.string('net');
+    table.integer('wgPort');
 }
